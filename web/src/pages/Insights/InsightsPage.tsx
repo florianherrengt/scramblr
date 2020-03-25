0df83d213748eedab2aaf3d4897e16d62a8e298c
Florian Herrengt <florian.herrengt@gmail.com>
2020-03-25 15:32:39 +0000

    remove unused variables

--- web/src/pages/Insights/InsightsPage.tsx
+++ web/src/pages/Insights/InsightsPage.tsx
@@ -5,50 +5,6 @@ import { fetchInsights } from '../../actions';
 import { RootState } from '../../reducers';
 import { CircularProgress } from '@material-ui/core';
 
-const data = [
-    {
-        label: 'Page A',
-        positive: 4000,
-        negative: 2400,
-        amt: 2400,
-    },
-    {
-        label: 'Page B',
-        positive: 3000,
-        negative: 1398,
-        amt: 2210,
-    },
-    {
-        label: 'Page C',
-        positive: 2000,
-        negative: 9800,
-        amt: 2290,
-    },
-    {
-        label: 'Page D',
-        positive: 2780,
-        negative: 3908,
-        amt: 2000,
-    },
-    {
-        label: 'Page E',
-        positive: 1890,
-        negative: 4800,
-        amt: 2181,
-    },
-    {
-        label: 'Page F',
-        positive: 2390,
-        negative: 3800,
-        amt: 2500,
-    },
-    {
-        label: 'Page G',
-        positive: 3490,
-        negative: 4300,
-        amt: 2100,
-    },
-];
 export const InsightsPage: React.SFC<{}> = () => {
     const dispatch = useDispatch();
     const insights = useSelector((state: RootState) => state.insights);
